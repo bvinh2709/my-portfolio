9f8f4dc4bfa9b22d74b5bb19013de6ae07f4db26
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-06-13 10:20:45 -0500

    added burger menu

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -1,11 +1,114 @@
-import { Box, Button, useMediaQuery } from "@mui/material"
+import { Box, Button, IconButton, useMediaQuery, Menu, MenuItem } from "@mui/material"
 import { useNavigate } from "react-router-dom"
+import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from 'react';
+
 
 const Navbar = () => {
     const navigate = useNavigate()
     const isMobile = useMediaQuery('(max-width: 600px)')
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const open = Boolean(anchorEl);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleStory = () => {
+        setAnchorEl(null);
+        navigate('/who')
+    }
+
+    const handleWorks = () => {
+        setAnchorEl(null);
+        navigate('/works')
+    }
+
+    const handleContact = () => {
+        setAnchorEl(null);
+        navigate('/contact')
+    }
+
     return (
-        <Box
+        isMobile ? (
+            <Box
+            display='flex'
+            justifyContent="space-between"
+            padding="0.5rem 0"
+            maxHeight='80%'
+            width='100%'
+            maxWidth='100%'
+        >
+            <Box
+                display='flex'
+                alignItems='center'
+                gap='0.5rem'
+            >
+                <Button onClick={() => navigate('/')}>
+                    <img src='./img/FullLogo_Transparent_NoBuffer.png' alt='Brand logo'
+                    height='120rem'
+                    />
+                </Button>
+                <Box
+                    display='flex'
+                    alignItems='center'
+                    padding='3rem'
+                    justifyContent='center'
+                >
+                    <Button
+                        sx={{backgroundColor:'lightyellow', color:'black', borderRadius: '0.5rem'}}
+                        onClick={() => navigate('/contact')}
+                    >Hire Me!</Button>
+                </Box>
+            </Box>
+            <IconButton
+                sx={{color:'lightyellow'}}
+                id="burger-menu"
+                aria-label="menu"
+                aria-controls="dropdown-menu"
+                aria-haspopup="true"
+                onClick={handleMenuOpen}
+            >
+                <MenuIcon sx={{ height:'3rem', width:'3rem'}}/>
+            </IconButton>
+            <Menu
+                id="dropdown-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleMenuClose}
+                PaperProps={{
+                    style: {
+                      marginTop: '-2.5rem'
+                    }
+                  }}
+            >
+                <MenuItem
+                    onClick={handleStory}
+                    sx={{color:'black', width: '100%'}}
+                >
+                    Story
+                </MenuItem>
+                <MenuItem
+                    onClick={handleWorks}
+                    sx={{color:'black', width: '100%'}}
+                >
+                    Works
+                </MenuItem>
+                <MenuItem
+                    onClick={handleContact}
+                    sx={{color:'black', width: '100%'}}
+                >
+                    Contact
+                </MenuItem>
+            </Menu>
+      </Box>
+        ) : (
+            <Box
             display='flex'
             justifyContent="space-between"
             padding="0.5rem 0"
@@ -62,6 +165,8 @@ const Navbar = () => {
                     >Hire Me!</Button>
                 </Box>
       </Box>
+        )
+
     )
   }
 
